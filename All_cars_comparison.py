ebb8f0ce00185df7f9322528772dd5cdedbadbfc
TommiWe <tommi.weck@gmail.com>
2025-09-17 15:48:23 +0300

    Add files via upload

---
+++ All_cars_comparison.py
@@ -0,0 +1,31 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+models = pd.read_csv('Price_table.csv')
+models = models.drop("Genmodel_ID", axis=1).dropna()
+cars = pd.read_csv('Ad_table.csv')
+cars = cars[["Maker", "Genmodel", "Reg_year", "Price", "Runned_Miles", "Adv_year"]].dropna()
+#cars = cars[cars["Maker"] == "Ford"]
+#cars = cars[cars["Genmodel"] == "Ka"]
+#cars = cars[cars["Reg_year"] < 2008]
+models_dict = {}
+for _, row in models.iterrows():
+    models_dict[(row["Maker"], row["Genmodel"], row["Year"])] = int(row["Entry_price"])
+price_percent = []
+miles = []
+age = []
+for _, row in cars.iterrows():
+    if (row["Maker"], row["Genmodel"], row["Reg_year"]) in models_dict:
+        try:
+            x = (int(row["Price"]), int(row["Runned_Miles"]))
+            price_percent.append(int(row["Price"]) / models_dict[(row["Maker"], row["Genmodel"], row["Reg_year"])])
+            miles.append(int(row["Runned_Miles"]))
+            age.append(int(row["Adv_year"])-int(row["Reg_year"]))
+        except:
+            pass
+plt.plot(age, price_percent, 'o', alpha=0.01)
+plt.xlim(0, 20)
+plt.ylim(0, 1.2)
+plt.show()
+    
